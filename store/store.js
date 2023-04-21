29b82d814561f0b0eea05fde2b0f3b9892b5c9a7
Luis F. Bravo <lbravo@abssa.com.mx>
2023-04-21 10:37:24 -0600

    Deleted some unnecesary files created for development

--- store/store.js
+++ store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterSlice'
 import editUserReducer from './editUserSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     editUserData: editUserReducer,
   },
 })
